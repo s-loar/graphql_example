c9037ebefc342b396eae5fd6516e7c394d45aefa
s-loar <sjl7909@gmail.com>
2022-12-05 16:33:45 -0500

    Initial files with user and movie types

---
+++ app/graphql/types/movie_type.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Types
+  class MovieType < Types::BaseObject
+    field :id, ID, null: false
+    field :user_id, Integer, null: false
+    field :title, String
+    field :year, Integer
+    field :genre, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :movies_count, Integer, null: true
+
+    def movies_count
+      object.movies.size
+    end
+  end
+end
